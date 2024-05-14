e6ff023bcead2c8f1a5ae2fe7be6f1c862cf5c28
Marco Schivo <marcoschivo1@gmail.com>
2024-05-14 23:21:53 +0200

    cleaning main

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import yfinance as yf
 import os
 import datetime
 
-
 # Specify the directory you want to iterate through
 indices_folder = 'market-indices'
 
@@ -21,31 +20,34 @@ for file_name in os.listdir(indices_folder):
     market_index.sort_values('Date', inplace=True)
     market_index.set_index('Date', inplace=True)
 
-    # print(market_index.head())
-
     # Calculate the rolling maximum (all-time high) to each point
     market_index['All_Time_High'] = market_index['Price'].cummax()
-
-    # Considering every 4 weeks we receive the paycheck variables holds
-    # how many salaries were received during the whole period.
-    # = (last day - first day) / 28 days
-    num_of_salaries = \
-        int((market_index.index[-1] - market_index.index[0]).days / 28)
-
+    
     # last price of the index
     last_price = market_index.iloc[-1][['Price']].to_numpy()
+    last_date  = market_index.iloc[-1].name
+
+    dca = 0
+    prev_date = market_index.index[0] # day of start working, 
+                                      # first salary in 28 days
+    for index, row in market_index.iterrows():
+        distance = (index - prev_date).days
+        if distance > 28: # salary every 4 weeks
+            dca += last_price / row['Price']
+            prev_date = index - datetime.timedelta(days=distance % 28)
 
     # Identify points where the price dropped 5%, 10%, 15% and 20%
     # market_index.astype({'Price': 'float32'})
     market_index['Drop_5_Percent'] = \
         market_index['Price'] <= (market_index['All_Time_High'] * 0.95)
-    # market_index['Drop_10_Percent'] = \
-    #     market_index['Price'] <= (market_index['All_Time_High'] * 0.90)
-    # market_index['Drop_15_Percent'] = \
-    #     market_index['Price'] <= (market_index['All_Time_High'] * 0.85)
-    # market_index['Drop_20_Percent'] = \
-    #     market_index['Price'] <= (market_index['All_Time_High'] * 0.80)
-    
+    market_index['Drop_10_Percent'] = \
+        market_index['Price'] <= (market_index['All_Time_High'] * 0.90)
+    market_index['Drop_15_Percent'] = \
+        market_index['Price'] <= (market_index['All_Time_High'] * 0.85)
+    market_index['Drop_20_Percent'] = \
+        market_index['Price'] <= (market_index['All_Time_High'] * 0.80)
+
+
     unique_drops = market_index[['All_Time_High','Drop_5_Percent']] \
                    .drop_duplicates(keep='first')
     unique_drops = unique_drops[unique_drops['Drop_5_Percent'] == True]
@@ -59,33 +61,20 @@ for file_name in os.listdir(indices_folder):
     for index, row in unique_drops.iterrows():
         distance = (index - prev_date).days
         if distance > 28: # salary every 4 weeks
+            # operation is a dict where keys are dates and values and 
+            # (price, num of salaries accumulated during the last period)
             operations[index] = (row['Price'], int(distance / 28))
             prev_date = index - datetime.timedelta(days=distance % 28)
 
-    sum = 0
+    # sum adds how much each operation have valuated till the end  
+    market_timing = 0
     for key in operations:
-        sum += last_price / operations[key][0] * operations[key][1]
+        market_timing += last_price / operations[key][0] * operations[key][1]
 
-    print('', sum / num_of_salaries - 1)
+    unused_salaries = int((last_date - prev_date).days / 28)
 
-    # prev_date = market_index.index[0]
-    # del operations
-    # operations = {}
-    # for index, row in market_index.iterrows():
-    #     distance = (index - prev_date).days
-    #     if distance > 28: # salary every 4 weeks
-    #         operations[index] = (row['Price'], int(distance / 28))
-    #         prev_date = index
-
-    # sum = 0
-    # for key in operations:
-    #     sum += last_price / operations[key][0] * operations[key][1]
-
-    # print('tva', sum / num_of_salaries - 1)
-
-    # dates = pd.Series(market_index.index)
-    # first_day_of_month = dates.groupby([dates.dt.year, dates.dt.month]).min().reset_index(drop=True)
-    # relationship = last_day_price / market_index.loc[first_day_of_month,'Price']
+    print('market timing / dca = ' + str(market_timing / dca))
+    print('unused salaries in market timing = ', unused_salaries)
 
     # Plotting both datasets
     plt.figure(figsize=(14, 7))
@@ -98,6 +87,5 @@ for file_name in os.listdir(indices_folder):
     plt.legend()
     plt.xticks(rotation=45)
     plt.tight_layout()
-
 
     plt.show()
