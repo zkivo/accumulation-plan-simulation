9c80566870f5b43a143676c18085fe3a75904d17
Marco Schivo <marcoschivo1@gmail.com>
2024-06-08 16:11:07 +0200

    processing all drops for every index

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+from pprint import pprint
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 import os
+import gc
 import datetime
 
 # Specify the directory you want to iterate through
 indices_folder = 'market-indices'
 
+list_results = []
+
 # Loop through each file in the directory
 for file_name in os.listdir(indices_folder):
     file_path = os.path.join(indices_folder, file_name)
     if not os.path.isfile(file_path): continue
-    print(f'Processing file: {file_name}')
+    print(f'\nProcessing file: {file_name}')
+
+    list_results.append({'file_name': file_name})
 
     # Reading the csv file and store it in a pandas dataframe
     market_index = pd.read_csv(file_path)
@@ -47,39 +53,60 @@ for file_name in os.listdir(indices_folder):
     market_index['Drop_20_Percent'] = \
         market_index['Price'] <= (market_index['All_Time_High'] * 0.80)
 
+    list_drop_strings = ['Drop_5_Percent', 'Drop_10_Percent', 'Drop_15_Percent', 'Drop_20_Percent']
 
-    unique_drops = market_index[['All_Time_High','Drop_5_Percent']] \
-                   .drop_duplicates(keep='first')
-    unique_drops = unique_drops[unique_drops['Drop_5_Percent'] == True]
-    unique_drops = unique_drops.merge(market_index, on='Date', how='inner')
-
-    # calculating for each drop how much investing in the index
-    # considering that the paycheck is received every 28 days
-    operations = {}
-    prev_date = market_index.index[0] # day of start working, 
-                                      # first salary in 28 days
-    for index, row in unique_drops.iterrows():
-        distance = (index - prev_date).days
-        if distance > 28: # salary every 4 weeks
-            # operation is a dict where keys are dates and values and 
-            # (price, num of salaries accumulated during the last period)
-            operations[index] = (row['Price'], int(distance / 28))
-            prev_date = index - datetime.timedelta(days=distance % 28)
-
-    # sum adds how much each operation have valuated till the end  
-    market_timing = 0
-    for key in operations:
-        market_timing += last_price / operations[key][0] * operations[key][1]
-
-    unused_salaries = int((last_date - prev_date).days / 28)
-
-    print('market timing / dca = ' + str(market_timing / dca))
-    print('unused salaries in market timing = ', unused_salaries)
-
-    # Plotting both datasets
+    # Preparing plot of the figure
     plt.figure(figsize=(14, 7))
     plt.plot(market_index.index, market_index['Price'], label=file_name, color='blue')
-    plt.plot(unique_drops.index, unique_drops['Price'], 'r.', markersize=10, label='5% Drop from All-Time High')
+    for string in list_drop_strings:
+        # print("\tresults for ", string)
+        try:
+            del unique_drops
+            gc.collect() # garbage collector
+        except:
+            pass        
+        try:
+            del operations
+            gc.collect() # garbage collector
+        except:
+            pass
+
+        unique_drops = market_index[['All_Time_High',string]] \
+                    .drop_duplicates(keep='first')
+        unique_drops = unique_drops[unique_drops[string] == True]
+        unique_drops = unique_drops.merge(market_index, on='Date', how='inner')
+
+        # calculating for each drop how much investing in the index
+        # considering that the paycheck is received every 28 days
+        operations = {}
+        prev_date = market_index.index[0] # day of start working, 
+                                        # first salary in 28 days
+        for index, row in unique_drops.iterrows():
+            distance = (index - prev_date).days
+            if distance > 28: # salary every 4 weeks
+                # operation is a dict where keys are dates and values:
+                # (price, num of salaries accumulated during the last period)
+                operations[index] = (row['Price'], int(distance / 28))
+                prev_date = index - datetime.timedelta(days=distance % 28)
+
+        # sum adds how much each operation have valuated till the end  
+        market_timing = 0
+        for key in operations:
+            market_timing += last_price / operations[key][0] * operations[key][1]
+
+        unused_salaries = int((last_date - prev_date).days / 28)
+
+        performance_strategy = round((((market_timing / dca) - 1) * 100)[0].item(), 2)
+
+        # print(type(performance_strategy))
+
+        # print('market timing / dca = ', str(performance_strategy), '%')
+        # print('unused salaries in market timing = ', unused_salaries)
+
+        list_results[-1][string] = {'mt / dca': performance_strategy,
+                                    'unused_salaries' : unused_salaries}
+
+        plt.plot(unique_drops.index, unique_drops['Price'], '.', markersize=10, label=string)
     # plt.plot(first_day_of_month, market_index.loc[first_day_of_month,'Price'], 'g.', markersize=10, label='5% Drop from All-Time High')
     plt.title(file_name)
     plt.xlabel('Date')
@@ -88,4 +115,5 @@ for file_name in os.listdir(indices_folder):
     plt.xticks(rotation=45)
     plt.tight_layout()
 
-    plt.show()
+pprint(list_results)
+plt.show()
